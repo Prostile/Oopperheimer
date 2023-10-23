22893df8a70c98a06016b5dfd243c9715dcc0190
Prostile <128052016+Prostile@users.noreply.github.com>
2023-10-23 23:01:14 +0700

    Update MassTemp.cpp

--- MassTemp.cpp
+++ MassTemp.cpp
@@ -1,4 +1,4 @@
-#include "MassiveTemp.h"
+#include "Source.h"
 #include <iostream>
 
 int main()
@@ -22,8 +22,8 @@ int main()
 
 	std::cout << "3:" << std::endl;
 	std::cout << "int: " << std::endl;
-	if (Aint == Bint) std::cout << "ðàâíû" << std::endl;
-	else std::cout << "íå ðàâíû" << std::endl;
+	if (Aint == Bint) std::cout << "равны" << std::endl;
+	else std::cout << "не равны" << std::endl;
 	//4)	Измените mas1 добавив в него число 6. Выведите измененный mas1 на экран.Сравните массивы mas1 и mas2.
 
 	std::cout << "4:" << std::endl;
@@ -32,43 +32,44 @@ int main()
 	Aint.Print();
 
 	std::cout << "int: " << std::endl;
-	if (Aint == Bint) std::cout << "ðàâíû" << std::endl;
-	else std::cout << "íå ðàâíû" << std::endl;
+	if (Aint == Bint) std::cout << "равны" << std::endl;
+	else std::cout << "не равны" << std::endl;
 	//5)	Создайте объект Array mas3, который получается добавлением к mas1  числа 7. Выведите mas3 на экран.
 
 	std::cout << "5:" << std::endl;
-	TempArr<int> Cint = Aint += 7;
+	Aint += 7;
+	TempArr<int> Cint = Aint;// += 7;
 	Cint.Print();
 	//6)	Удалите первый элемент из mas3.Выведите получившийся mas3 на экран.
 	std::cout << "6:" << std::endl;
 
-	Cint -= 0;
+	Cint.DelPosEq(1);
 
 	Cint.Print();
 	//7)	Создайте массив mas4, который получается из mas3 удалением последнего элемента.Выведите получившийся mas4 на экран.
 	std::cout << "7:" << std::endl;
 	int lastInt = Cint.GetN() - 1;
-		//std::cout << lastInt << " " << lastFloat << " " << lastChar;
+	//std::cout << lastInt << " " << lastFloat << " " << lastChar;
 
 	Cint -= lastInt;
 	TempArr<int> Dint = Cint;
 
 	Dint.Print();
 	//8)	Удалите из mas1 элемент key = 1. Проверьте, равны ли mas1 и mas4
-	
+
 	Aint -= Aint.FindKey(1);
 
-	if (Aint == Dint) std::cout << "ðàâíû" << std::endl;
-	else std::cout << "íå ðàâíû" << std::endl;
+	if (Aint == Dint) std::cout << "равны" << std::endl;
+	else std::cout << "не равны" << std::endl;
 	//9)	Введите число n  и сформируйте массив b из n случайных чисел.Создайте объект Array mas5, 
 	//		используя конструктор с аргументом - массив.
 	int n = -1;
 
 	while (n < 0)
 	{
-		std::cout << "ââåäèòå ÷èñëî n ýëåìåíòîâ: " << std::endl;
+		std::cout << "введите число n элементов: " << std::endl;
 		std::cin >> n;
-		if (n < 0) std::cout << "÷èñëî ìåíüøå 0" << std::endl;
+		if (n < 0) std::cout << "число меньше 0" << std::endl;
 	}
 
 	int* bInt = new int[n];
@@ -81,10 +82,10 @@ int main()
 
 	Eint.Print();
 	//10)  Найдите максимальный и минимальный элемент в mas5.Выведите на экран эти элементы и их индексы.
-	 
+
 	std::cout << "max: " << Eint[Eint.Max()] << " min: " << Eint[Eint.Min()] << std::endl;
 	//11)	Сортируйте mas5 и выведите результат на экран.
-	
+
 	Eint.Sorting();
 
 	Eint.Print();
@@ -95,31 +96,31 @@ int main()
 	Fint.Print();
 	//13)  Создайте  объект Array mas7 – массив из 4 - х элементов.
 	//Заполните член - данные(10, 11, 12, 13), используя потоковый ввод.
-	
+
 	TempArr<int> Gint(4);
-	std::cout << "ââåäèòå 4 ýëåìåíòà: ";
+	std::cout << "введите 4 элемента: ";
 	for (int i = 0; i < 4; i++)
 	{
 		int n; std::cin >> n; Gint[i] = n;
 	}
 	Gint.Print();
 	//14)  Проверьте, входят ли в mas7 элементы 1 и 10. Создайте mas8, который получается из mas7 удалением элемента 10.
-	
-	if (Gint.FindKey(1) != -1) std::cout << "1 âõîäèò â ìàññèâ" << std::endl;
-	else std::cout << "1 íå âõîäèò â ìàññèâ" << std::endl;
-	if(Gint.FindKey(10) != -1) std::cout << "10 âõîäèò â ìàññèâ" << std::endl;
-	else std::cout << "10 íå âõîäèò â ìàññèâ" << std::endl;
+
+	if (Gint.FindKey(1) != -1) std::cout << "1 входит в массив" << std::endl;
+	else std::cout << "1 входит в массив" << std::endl;
+	if (Gint.FindKey(10) != -1) std::cout << "10 не входит в массив" << std::endl;
+	else std::cout << "10 не входит в массив" << std::endl;
 	TempArr<int> Hint = Gint - 10;
 	Hint.Print();
 	//15)	Измените mas4, добавив к нему mas7.Выведите результат на экран.Сравните mas6 и mas4.
-	
+
 	Dint += Hint;
 	Dint.Print();
 
 
 
 	//16) Осуществите присвоение mas4 = mas6.Выведите результат на экран
-	 
+
 	Dint = Fint;
 	Dint.Print();
 
